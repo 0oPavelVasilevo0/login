26fdedbda152835a2bcbd9fdb2b74d6925422b7c
Pavel Vasilev <karterpahan@gmail.com>
2024-02-09 19:19:14 +0100

    FacebookButton

--- client/src/components/LoginWindow.jsx
+++ client/src/components/LoginWindow.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../pngegg.png'
-import Btn from './Btn'
+// import Btn from './Btn'
 import BtnForgot from './BtnForgot'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { GoogleLogin } from '@react-oauth/google'
+import { LoginSocialFacebook } from 'reactjs-social-login'
+import { FacebookLoginButton } from 'react-social-login-buttons'
 // import { useIntl } from 'react-intl'
 // import { Intl } from 'intl'
 // import { DisplayNames } from '@formatjs/intl-displaynames'
@@ -16,8 +18,26 @@ import { GoogleLogin } from '@react-oauth/google'
 const LoginWindow = () => {
   const navigate = useNavigate();
   // const intl = useIntl(); // Initialize intl
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  // const [profile, setProfile] = useState(null);
 
-//*********        NAME 
+  const isDisabled = login === '' || password === '';
+
+  const handleLoginChange = (event) => {
+    setLogin(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Действия при отправке формы
+  };
+
+  //*********        NAME 
 
   // const handleLoginSuccess = (credentialResponse) => {
   //   const { credential } = credentialResponse;
@@ -28,7 +48,7 @@ const LoginWindow = () => {
   //   navigate('/userinfo', { state: { name } }); // Redirect to UserInfo page
   // };
 
-//*****************      EMAIL
+  //*****************      EMAIL
 
   // const handleLoginSuccess = (credentialResponse) => {
   //   const { credential } = credentialResponse;
@@ -44,10 +64,10 @@ const LoginWindow = () => {
   //   navigate('/userinfo', { state: { email } }); // Redirect to UserInfo page
   // };
 
-//         ******    *******     ******    ***
+  //         ******    *******     ******    ***
 
-//***************    вариант с обработкой ошибок - рабочий
-  
+  //***************    вариант с обработкой ошибок - рабочий
+
   // const handleLoginSuccess = (credentialResponse) => {
   //   const { credential } = credentialResponse;
 
@@ -63,9 +83,9 @@ const LoginWindow = () => {
   //     console.error('Failed to decode credential:', error);
   //   }
   // };
-//          ********   *********  **********   ********   ****
+  //          ********   *********  **********   ********   ****
 
-//новый вариант ::::::::::::::::::::::::::::::::::::::::::::::::::
+  //новый вариант ::::::::::::::::::::::::::::::::::::::::::::::::::
 
   const handleLoginSuccess = (credentialResponse) => {
     const { credential } = credentialResponse;
@@ -79,7 +99,7 @@ const LoginWindow = () => {
   };
 
 
-//    ****************  с бибилиотекой Intl(многоязычность)
+  //    ****************  с бибилиотекой Intl(многоязычность)
 
   // const handleLoginSuccess = (credentialResponse) => {
   //   const { credential } = credentialResponse;
@@ -101,31 +121,28 @@ const LoginWindow = () => {
   //   }
   // };
 
-       // второй вариант с обработкой ошибок(только для латиницы)
+  // второй вариант с обработкой ошибок(только для латиницы)
 
-//     const { credential } = credentialResponse;
-//     if (!credential) {
-//       console.error("Credential not found in response");
-//       return;
-//     }
+  //     const { credential } = credentialResponse;
+  //     if (!credential) {
+  //       console.error("Credential not found in response");
+  //       return;
+  //     }
 
-//   try {
-//     const { name } = credential ? JSON.parse(atob(credential.split('.')[1])) : {};
-//     console.log(name);
-//     navigate('/userinfo', { state: { name } });
-//   } catch (error) {
-//     console.error("Error decoding credential:", error);
-//   }
-// }
-
+  //   try {
+  //     const { name } = credential ? JSON.parse(atob(credential.split('.')[1])) : {};
+  //     console.log(name);
+  //     navigate('/userinfo', { state: { name } });
+  //   } catch (error) {
+  //     console.error("Error decoding credential:", error);
+  //   }
+  // }
 
   const handleLoginError = () => {
     console.log('Login Failed');
   };
 
-
   return (
-
     <div className='container'>
       <div className='login_window'>
         <div className='login_header'>
@@ -134,25 +151,25 @@ const LoginWindow = () => {
             Welcome
           </h1>
         </div>
-        <form className='login_form'>
+        <form className='login_form' onSubmit={handleSubmit}>
           <div className='login_input'>
             <label htmlFor='login'>Login</label>
-            <input name='login' type='text' placeholder='Enter login' />
+            <input name='login' type='text' placeholder='Enter login' value={login} onChange={handleLoginChange} />
           </div>
           <div className='password_input'>
             <label htmlFor='password'>Password</label>
-            <input name='password' type="password" placeholder='Enter password' />
+            <input name='password' type="password" placeholder='Enter password' value={password} onChange={handlePasswordChange} />
           </div>
           <div className='btn_section'>
             <NavLink to='/pageforgot' className='nav'>
               <BtnForgot />
             </NavLink>
-            <Btn />
+            <button className={`btn ${isDisabled ? 'disabled' : ''}`} type='submit' disabled={isDisabled}>Login</button>
+            {/* <Btn /> */}
           </div>
         </form>
       </div>
       <div className='btn_goggle'>
-
         <GoogleLogin
           // clientId="<YOUR_CLIENT_ID>"
           environment="TEST"
@@ -164,27 +181,53 @@ const LoginWindow = () => {
           onSuccess={handleLoginSuccess}
           onError={handleLoginError}
         />
-
-        {/* <GoogleLogin
-          environment="TEST"
-          theme='outline'
-          text='signin_with'
-          size='medium'
-          width='302'
-          locale='en_EN'
-          onSuccess={onSuccess}
-          onError={(error) => console.error('Login Failed:', error)}
-          /> */}
-
-
-
-
-
-
-      </div>
-
+       {/* {!profile? */}
+         <LoginSocialFacebook
+          appId='284162668018548'
+         
+          onResolve={(response) => {
+            console.log(response)
+            // setProfile(response.data)
+          }}
+          onReject={(error) => {
+            console.log(error)
+          }}
+        >
+          <FacebookLoginButton
+          size='32px'
+          iconSize='16px'
+            iconColor='dark'
+            text='signin with Facebook'
+            align='center'
+           
+            // iconFormat={'medium'}
+            className='btn_facebook'
+            activeStyle={{ background: '#0077ff08', border: '1px #0077ff08'}}
+            // background='dark'
+            // boxShadow='none'
+          style={{
+            width: '302px',
+              // fontFamily: 'Google Sans',
+            fontSize: '14px',
+            // fontWidth: '200',
+            background: 'white',
+            // theme: 'outline',
+            color: '#3c4043',
+            letterSpacing: '0.45px',
+            
+          }}
+            // onSuccess={handleLoginSuccess}
+            // onError={handleLoginError}
+           />
+           
+        </LoginSocialFacebook>
+        {/* : ''} */}
+        {/* {profile? <div>
+          <h1>{profile.name}</h1>
+          <img src={profile.picture.data.url} alt='profile_picture'/>
+      </div>: ''} */}
     </div>
-
+    </div>
   )
 }
 
